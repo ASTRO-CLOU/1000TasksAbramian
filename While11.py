414dd8c4c931eeeca705c4619ead117290a39578
ASTRO-CLOU <85131744+ASTRO-CLOU@users.noreply.github.com>
2026-09-17 11:35:08 +0300

    Add files via upload

---
+++ While11.py
@@ -0,0 +1,20 @@
+try:
+    N = int(input("Введите целое число N: "))
+    
+    if N <= 0:
+        print("Число должно быть больше 0")
+    else:
+        K = 1 
+        sum = 0 
+        
+        while sum <= N:
+            
+            for i in range(1, K + 1):
+                sum += i
+            K += 1
+    
+        print(f"Наименьшее K: {K}")
+        print(f"Сумма: {sum}")
+
+except ValueError:
+    print("Ошибка: введено не целое число.")
