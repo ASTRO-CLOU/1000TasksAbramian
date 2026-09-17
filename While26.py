463de600cc586d6bff89fd5283c3c7f411d93bcb
ASTRO-CLOU <85131744+ASTRO-CLOU@users.noreply.github.com>
2026-09-17 16:50:20 +0300

    Add files via upload

---
+++ While26.py
@@ -0,0 +1,12 @@
+try:
+    N = int(input())
+    F1 = 1
+    F2 = 1
+    while F2 < N:
+        F = F1 + F2
+        F1 = F2
+        F2 = F
+    print(F1)
+    print(F1 + F2)
+except ValueError as e:
+    print(f"произошла ошибка: {e}")
