419a10a33dbff5aae7bf8215423d1466d9ae7fb9
ASTRO-CLOU <85131744+ASTRO-CLOU@users.noreply.github.com>
2026-09-17 16:42:53 +0300

    Add files via upload

---
+++ While20.py
@@ -0,0 +1,13 @@
+try:
+    N = int(input())
+    found = False
+    while N > 0:
+        if N % 10 == 2:
+            found = True
+        N //= 10
+    if found:
+        print("TRUE")
+    else:
+        print("FALSE")
+except ValueError as e:
+    print(f"произошла ошибка: {e}")
