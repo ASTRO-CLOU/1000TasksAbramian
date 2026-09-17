e75de7cdcb06aca9b81aa187e19d410eac507ac7
ASTRO-CLOU <85131744+ASTRO-CLOU@users.noreply.github.com>
2026-09-17 12:50:24 +0300

    Add files via upload

---
+++ While14.py
@@ -0,0 +1,21 @@
+try:
+    A = int(input("Введите целое число A: "))
+    
+    if A <= 0:
+        print("Число должно быть больше 0")
+    else:
+        K = 1
+        sum = 0.0
+        
+        while K <= A:
+            
+            K += 1
+            sum += 1/K
+        
+        K -+ 1
+        sum -= 1/K
+        
+        print(f"Наибольшее K: {K}")
+
+except ValueError:
+    print("Ошибка: введено не целое число.")
